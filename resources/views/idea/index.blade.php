a0e6aa6bf1591c80c03064b59ae32a64da36377c
Chase Terry <chase.terry87@gmail.com>
2021-12-12 18:31:47 -0600

    Added alpine logic to click the idea box and redirect to idea page while ignoring other elements in the box

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -30,7 +30,15 @@
 
     <div class="ideas-container space-y-6 my-6 hover:shadow-card transition duration-150 ease-in">
         @foreach($ideas as $idea)
-            <div class="idea-container bg-white rounded-xl flex cursor-pointer">
+            <div
+                x-data
+                @click="const target = $event.target.tagName.toLowerCase()
+                const ignores = ['button','svg','path','a', 'img']
+                const ideaLink = $event.target.closest('.idea-container').querySelector('.idea-link')
+
+                !ignores.includes(target) && ideaLink.click()"
+                class="idea-container bg-white rounded-xl flex cursor-pointer"
+            >
                 <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                     <div class="text-center">
                         <div class="font-semibold text-2xl">12</div>
@@ -49,7 +57,7 @@
                     </div>
                     <div class="mx-4 w-full flex flex-col justify-between">
                         <h4 class="text-xl font-semibold mt-2 md:mt-0">
-                            <a href="{{ route('idea.show', $idea) }}" class="hover:underline">{{ $idea->title }}</a>
+                            <a href="{{ route('idea.show', $idea) }}" class="idea-link hover:underline">{{ $idea->title }}</a>
                         </h4>
                         <div class="text-gray-600 mt-3 line-clamp-3">
                             {{ $idea->description }}
@@ -62,19 +70,19 @@
                                 <div>&bull;</div>
                                 <div class="text-gray-900">3 Comments</div>
                             </div>
-                            <div 
+                            <div
                                 class="flex items-center space-x-2 mt-4 md:mt-0"
                                 x-data="{ isOpen: false }"
                             >
                                 <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
-                                <button 
+                                <button
                                     @click="isOpen = !isOpen"
                                     class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3 border"
                                 >
                                     <svg fill="currentColor" class="text-gray-400" width="24" height="6">
                                         <path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z">
                                         </path>
-                                    </svg>  
+                                    </svg>
                                     <ul
                                         x-cloak
                                         x-show="isOpen"
