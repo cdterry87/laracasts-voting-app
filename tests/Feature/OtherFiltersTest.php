259b762a25bf1928a042568b86e4ca23ed1863f3
Chase Terry <cterry@lightbeacon.com>
2021-12-17 10:58:26 -0600

    Added spam and spam filters tests

--- tests/Feature/OtherFiltersTest.php
+++ tests/Feature/OtherFiltersTest.php
@@ -229,4 +229,35 @@ class OtherFiltersTest extends TestCase
             && $ideas->get(2)->title === 'My First Idea';
         });
     }
+
+    /** @test */
+    public function spam_ideas_filter_works()
+    {
+        $user = User::factory()->create([
+          'email' => 'chase.terry87@gmail.com'
+        ]);
+        $ideaOne = Idea::factory()->create([
+            'title' => 'Idea One',
+            'spam_reports' => 1
+        ]);
+        $ideaTwo = Idea::factory()->create([
+            'title' => 'Idea Two',
+            'spam_reports' => 2
+        ]);
+        $ideaThree = Idea::factory()->create([
+            'title' => 'Idea Three',
+            'spam_reports' => 3
+        ]);
+        $ideaFour = Idea::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(IdeasIndex::class)
+            ->set('filter', 'Spam Ideas')
+            ->assertViewHas('ideas', function ($ideas) {
+                return $ideas->count() === 3
+                    && $ideas->first()->title === 'Idea Three'
+                    && $ideas->get(1)->title === 'Idea Two'
+                    && $ideas->get(2)->title === 'Idea One';
+            });
+    }
 }
